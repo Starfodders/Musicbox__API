76575143b2b4bd03c5c3d4e953ad1915a404c1f2
Michael Deng <michaeldeng55@gmail.com>
2023-03-16 14:27:49 -0400

    middleware and CORS, port is 3000

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('/public'));
+app.use(express.json())
+app.use(cors({origin: 3000}))
+
 
 app.get('/', (req, res) => {
     res.send('http://localhost:8080/images/backgroundBars.avg');
+    console.log('here')
 })
 
 app.listen(8080, () => {
